66129c38393e4cd6ecc8e007cee1341e73a2d8d2
Cradirow <hanorang2@naver.com>
2020-08-23 05:34:23 +0000

    add basic ranking template

--- humba_django/humba/urls.py
+++ humba_django/humba/urls.py
@@ -3,8 +3,10 @@ from. import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('navigate/', views.navigate, name='navigator'),
     path('team/', views.team_table, name='team'),
     path('goal/', views.player_goal_table, name='goal'),
     path('assist/', views.player_assist_table, name='assist'),
-    path('schedule/<int:round_idx>', views.schedule_table, name='schedule'),
+    path('schedule/', views.schedule_table, name='schedule'),
+    path('schedule/<int:round_idx>', views.schedule_table_round, name='schedule_round'),
 ]
--- humba_django/humba/views.py
+++ humba_django/humba/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+#import requests
+import json
 
 def index(request):
     return HttpResponse("I'm humba!")
+
+@csrf_exempt
+def navigate(request):
+    requestBody = json.loads(request.body.decode('utf-8'))
+    #print(requestBody)
+
+    originParam = requestBody['action']['detailParams']['sys_humba_football']['origin']
+    systemParam = requestBody['action']['params']['sys_humba_football']
+
+    textBody = ""
+    if(systemParam == '득점'):
+        textBody = originParam
+    elif(systemParam == '도움'):
+        textBody = originParam
+    elif(systemParam == '팀'):
+        textBody = originParam
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                    "text": "%s 순위에 대해 알려줄게 :)" % textBody
+                    }
+                }
+            ]
+        }
+    }
+    return JsonResponse(responseBody, safe=False)
 
 def team_table(request):
     response = "This is team table."
@@ -16,6 +50,10 @@ def player_assist_table(request):
     response = "This is player assist table."
     return HttpResponse("Hi there! %s" % response)
 
-def schedule_table(request, round_idx):
+def schedule_table(request):
+    response = "This is schedule table."
+    return HttpResponse("Hi there! %s" % response)
+
+def schedule_table_round(request, round_idx):
     response = "This is %s round schedule table."
     return HttpResponse("Hi there! %s" % response % round_idx)
